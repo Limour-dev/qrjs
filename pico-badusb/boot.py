6e458c1854887dabf0434f08aee5d7c4904eaefa
Limour <93720049+Limour-dev@users.noreply.github.com>
2024-12-02 21:46:25 +0800

    禁用掉不必要的设备，避免严格限制驱动的内网系统驱动出错

--- pico-badusb/boot.py
+++ pico-badusb/boot.py
@@ -1,5 +1,12 @@
 import microcontroller
 from storage import disable_usb_drive
+from usb_cdc import disable as disable_usb_cdc
+from usb_midi import disable as disable_usb_midi
+import usb_hid
 microcontroller.on_next_reset(microcontroller.RunMode.SAFE_MODE)
 if(microcontroller.cpu.reset_reason != microcontroller.ResetReason.RESET_PIN):
     disable_usb_drive()
+    disable_usb_cdc()
+    disable_usb_midi()
+    usb_hid.disable()
+    usb_hid.enable((usb_hid.Device.KEYBOARD,))
